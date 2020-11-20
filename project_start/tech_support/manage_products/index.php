6a00bb12bc83e7c59ab2adaa197f2d1bcbaca0bc
nassarnour <42120101+nassarnour@users.noreply.github.com>
2020-11-19 21:42:18 -0500

    Add files via upload
    Project 3 part 1 done

---
+++ project_start/tech_support/manage_products/index.php
@@ -0,0 +1,68 @@
+<?php include '../view/header.php';
+require('../model/database.php');
+// require('../model/product_db.php');
+
+    $queryProducts = "SELECT * FROM products" ;
+    $execStatement = $db->prepare($queryProducts); //db object in database.php
+    $execStatement->execute(); //executes query, execstatment has all the data
+
+    $productsList = $execStatement->fetchAll(); // returns the rows. an array
+	$execStatement->closeCursor(); // done moving through db, have it pulled into memory; we are persisiting data
+
+?>
+
+<main>
+    <nav>
+    <h1>Product List</h1>
+    <table>
+    <tr>
+      <td> <strong> Code </strong></td>
+      <td><strong>Name</strong></td>
+      <td><strong>Version</strong></td>
+      <td><strong>Release Date</strong></td>
+    </tr>
+        <?php
+        foreach($productsList as $prodList) : ?>
+            <tr>
+                <td><?php echo $prodList['productCode'];?> </td>
+                <td><?php echo $prodList['name'];?> </td>
+                <td><?php echo $prodList['version'];?> </td>
+                <td><?php echo $prodList['releaseDate'];?> </td>
+                <td>
+                    <!-- <form id="mp_deleteForm" action="mp_delete.php" method="post" >
+                        <input type="button" value="Delete" id=mp_deleteBtn>
+                    </form> -->
+                    <!-- <button type="submit" method='mp_delete.php'>Delete</button> -->
+                    <form action="mp_delete.php" method="post">
+                     <input type="hidden" name="product_code"
+                        value="<?php echo $prodList['productCode']; ?>">
+                     <input type="submit" value="Delete">
+                    </form></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <br>
+    <a href="../add_product/add_product.php">Add Product</a>
+    <nav>
+</main>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Manage Products</title>
+</head>
+<body>
+    <h1>Product List</h1>
+    <table>
+
+    </table>
+
+    <a href="../add_product/add_product.php">Add Product</a>
+</body>
+</html> -->
+
+<?php include '../view/footer.php'; ?>
---
+++ project_start/tech_support/manage_products/mp_delete.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../model/database.php');
+
+$product_code = filter_input(INPUT_POST, 'product_code');
+
+
+
+    $query = 'DELETE FROM products
+    WHERE productCode= :product_code';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':product_code', $product_code);
+    $success = $statement-> execute();
+    $statement-> closeCursor();
+
+include('index.php');
+?>
